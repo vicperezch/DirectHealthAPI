18189f3326803ca1f3e5ed340d428c5463f643f8
Victor <vmpch1@outlook.com>
2024-09-10 21:11:39 -0600

    Proyecto creado

---
+++ src/main/java/com/uvg/directhealth/DirecthealthApplication.java
@@ -0,0 +1,13 @@
+package com.uvg.directhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirecthealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirecthealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uvg/directhealth/DirecthealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uvg.directhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirecthealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
